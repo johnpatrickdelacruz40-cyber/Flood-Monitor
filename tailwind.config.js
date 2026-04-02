a9c901d65dc0a3475a3325b6bd92c2d7687fc6de
Patrick <johnpatrickdelacruz40@gmail.com>
2026-04-02 16:10:43 +0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+export default {
+content: [
+"./index.html",
+"./src/**/*.{js,ts,jsx,tsx}",
+],
+theme: {
+extend: {
+colors: {
+cream: "#FDFBF7",
+sage: "#8B9A8B",
+charcoal: "#4A4A4A",
+terracotta: "#E3A88D"
+}
+},
+},
+plugins: [],
+}
